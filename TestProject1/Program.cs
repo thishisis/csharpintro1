048b6bb08cf470c9af68c696c7c74819c5bbd895
Android <student@read.me>
2026-01-28 01:54:33 +0200

    sync

--- TestProject1/Program.cs
+++ TestProject1/Program.cs
@@ -1,4 +1,9 @@
 ﻿Random dice = new Random();
 //#date and time down to the fraction of a millisecond and use that to seed an algorithm that produces a different number each time. 
-int roll = dice.Next(1, 7);
-Console.WriteLine(roll);
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(50, 101);
+
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
