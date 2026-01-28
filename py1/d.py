e8fb7f6e6b258c514fce0c0a076063109a6eca26
Android <student@read.me>
2026-01-28 15:09:11 +0200

    sync python

---
+++ py1/d.py
@@ -0,0 +1,51 @@
+class HashTable:
+    def __init__(self):
+        self.collection = {}
+
+    def hash(self, key):
+        return sum(ord(char) for char in key)
+    
+    def add(self, key, value):
+        hashed_key = self.hash(key)
+        if hashed_key in self.collection:
+            self.collection[hashed_key].append((key, value))
+        else:
+            self.collection[hashed_key] = [(key, value)]
+            
+    def remove(self, key):
+        hashed_key = self.hash(key)
+        if hashed_key in self.collection:
+            if len(self.collection[hashed_key]) > 1:
+                self.collection[hashed_key].pop()
+            else:
+                del self.collection[hashed_key]
+                
+    def lookup(self, key):
+        hashed_key = self.hash(key)
+        pair = self.collection.get(hashed_key, None)
+        if pair:
+            k, v = pair[-1]
+            return v
+        return None
+    
+if __name__ == "__main__":
+    ht = HashTable()
+    ht.add("apple", 1)
+    ht.add("banana", 2)
+    ht.add("apple", 3)
+    
+    print("apple:", ht.lookup("apple")) 
+    print("banana:", ht.lookup("banana"))
+    
+    ht.remove("apple")
+    print("apple:", ht.lookup("apple")) 
+    
+    ht.remove("apple")
+    print("apple:", ht.lookup("apple"))
+    
+    print(ht.remove("wrongkey")) 
+    
+    print("banana:", ht.lookup("banana"))
+    
+    ht.add('golf', 'sport')
+    print(ht.collection)
