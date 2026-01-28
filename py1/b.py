a475bd10ec1a6c6250049778f6fdfa6e661287cc
Android <student@read.me>
2026-01-28 14:35:21 +0200

    sync python

--- py1/b.py
+++ py1/b.py
@@ -36,4 +36,48 @@ class Category:
         return answer
 
 def create_spend_chart(categories):
-    pass
+    answer = "Percentage spent by category\n"
+    spent_amounts = []
+    total_spent = 0
+    for i in range(len(categories)):
+        spent = sum(-1*entry['amount'] for entry in categories[i].ledger if entry['amount'] < 0)
+        spent_amounts.append(spent)
+        total_spent += spent
+    spent_percentages = [int((spent / total_spent) * 10) * 10 for spent in spent_amounts]
+    for i in range(100, -1, -10):
+        answer += f"{i:>3}| "
+        for j in range(len(spent_percentages)):
+            if spent_percentages[j] >= i:
+                answer += "o  "
+            else:
+                answer += "   "
+        answer += "\n"
+    answer += "    " + "-" * (len(categories) * 3 + 1) + "\n"
+    max_length = max(len(category.name) for category in categories)
+    for i in range(max_length):
+        answer += "     "
+        for category in categories:
+            if i < len(category.name):
+                answer += category.name[i] + "  "
+            else:
+                answer += "   "
+        if i < max_length - 1:
+            answer += "\n"
+    return answer
+
+if __name__ == "__main__":
+    food = Category("Food")
+    entertainment = Category("Entertainment")
+    business = Category("Business")
+
+    food.deposit(900, "deposit")
+    food.withdraw(105.55, "groceries")
+    food.withdraw(33.40, "restaurant and more food for dessert")
+
+    entertainment.deposit(900, "deposit")
+    entertainment.withdraw(33.40, "movie ticket")
+    
+    business.deposit(900, "deposit")
+    business.withdraw(10.99, "office supplies")
+
+    print(create_spend_chart([business, food, entertainment]))
